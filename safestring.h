f1175e935f501808874d71a9737aabc11614bf5c
egjallow10 <egjallow10@gmail.com>
2025-08-30 19:46:47 +0000

    headers

--- safestring.h
+++ safestring.h
@@ -10,5 +10,11 @@ typedef struct {
 	size_t capacity;
 } SafeString;
 
+SafeString **ss_create(size_t capacity);
+void ss_free(SafeString *s);
+size_t ss_len(const SafeString *s);
+size_t ss_capacity(const SafeString *s);
+int ss_copy(SafeString *dest, const char *src);
+int ss_concat(SafeString *dest, const char *src);
 #endif
 
